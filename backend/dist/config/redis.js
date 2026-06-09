273687a1244ddece515fd13ee40f75d21f459641
davidmaiky <davidmaiky@hotmail.com>
2026-06-09 10:17:41 -0300

    Revert "inicio"
    This reverts commit d800e5c433a23be9f7838c48c44802509b465c14.

--- backend/dist/config/redis.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REDIS_OPT_LIMITER_DURATION = exports.REDIS_OPT_LIMITER_MAX = exports.REDIS_URI_CONNECTION = void 0;
-// Garante que a URI de conexão seja uma string.
-exports.REDIS_URI_CONNECTION = process.env.REDIS_URI || "";
-// Converte o valor para um número inteiro, com um padrão de 1.
-exports.REDIS_OPT_LIMITER_MAX = parseInt(process.env.REDIS_OPT_LIMITER_MAX || "1", 10);
-// Converte o valor para um número inteiro, com um padrão de 3000.
-exports.REDIS_OPT_LIMITER_DURATION = parseInt(process.env.REDIS_OPT_LIMITER_DURATION || "3000", 10);
--- backend/dist/controllers/VersionController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = void 0;
-const index = async (req, res) => {
-    return res.status(200).json({
-        version: "6.3.0"
-    });
-};
-exports.index = index;
